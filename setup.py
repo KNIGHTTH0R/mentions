c769e420bdeda4c1836b85b708c3c4f98a7c5065
Juan Olvera <olverajuanluis@gmail.com>
2015-02-14 23:56:42 -0600

    added  to the  file

--- setup.py
+++ setup.py
@@ -8,4 +8,9 @@ setup(name='mention',
       author_email='thinkxl@gmail.com',
       license='MIT',
       packages=['mention'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=[
+        'requests>=2.5.1',
+        'beautifulsoup4>=4.3.2'
+      ]
+)
