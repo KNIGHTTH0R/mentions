67ca11417f1583a57f56cca73f302e63369248ed
Juan Olvera <olverajuanluis@gmail.com>
2015-02-15 00:16:02 -0600

    typo fixed in __init__.py

--- mention/__init__.py
+++ mention/__init__.py
@@ -11,7 +11,7 @@ Get share counts from a given URL easily.
 """
 
 
-from karma import facebook_total_count, facebook_like_count, \
+from mention import facebook_total_count, facebook_like_count, \
                   facebook_comment_count, facebook_share_count, \
                   tweets, google_plus_one, linkedin_mentions, \
                   pinterest_shares, stumbleupon_views, reddit_mentions
